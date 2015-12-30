6c2cf45b0c0b135f588ee067a05258955bc19d09
Edward Smith <edwardmsmith@gmail.com>
2015-12-30 10:30:36 -0600

    Style change

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers'
